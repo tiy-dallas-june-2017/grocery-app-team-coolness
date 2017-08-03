eaaf6acc090b6dd0952fc9e0e3cd7db0ea38c8bd
austinChappell <Austin.W.Chappell@gmail.com>
2017-08-03 09:54:46 -0500

    mvc setup working

--- server.js
+++ server.js
@@ -3,7 +3,19 @@ const express = require('express'),
       bodyParser = require('body-parser'),
       mustacheExpress = require('mustache-express');
 
+const router = require('./routes/routes');
+
 let port = process.env.PORT || 3000;
+
+let mustacheInstance = mustacheExpress();
+mustacheInstance.cache = null;
+app.engine('mustache', mustacheInstance);
+
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+
+app.use(express.static('public'));
+app.use('/', router);
 
 app.listen(port, () => {
   console.log(`Your app is running on PORT ${ port }.`);
