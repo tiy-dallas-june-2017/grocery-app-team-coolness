29b1c065cbeca5ae129524f40e2657b860051a0b
austinChappell <Austin.W.Chappell@gmail.com>
2017-08-03 11:06:53 -0500

    get data on page

--- routes/routes.js
+++ routes/routes.js
@@ -12,4 +12,26 @@ router.get('/auditlog', (req, res) => {
   res.render('auditlog');
 });
 
+router.get('/currentinventory', (req, res) => {
+  let inventory = [
+    {
+      item: 'steak',
+      quantity: 45,
+      price: 8.99
+    },
+    {
+      item: 'banana',
+      quantity: 20,
+      price: 0.39
+    },
+    {
+      item: 'beer',
+      quantity: 37,
+      price: 10.99
+    }
+  ];
+  let data = { inventory }
+  res.render('current', data);
+})
+
 module.exports = router;
